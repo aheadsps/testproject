7a7f66e4a58f726691c12fc9996331265302cdf7
Maksim Boykov <mboykov6@gmail.com>
2025-03-04 00:34:02 +0300

    create pull

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.news import get_news
 
 def main():
     """Главная функция для работы приложения"""
-    articles = get_news('tesla', ['Twitter'])
+    articles = get_news('Tesla', ['Twitter'])
     print(articles)
 
 if __name__ == '__main__':
